e1621fa0a7f45058470aa53b00287d58f9d70d81
Theodor Holmberg <35803316+egeltorp@users.noreply.github.com>
2025-10-28 17:14:11 +0100

    Implement first game logic in __main__.py

---
+++ __main__.py
@@ -0,0 +1,39 @@
+# __main__.py
+# Theodor Holmberg 2025
+
+from io_cli import TextUI
+from game import WumpusGame
+
+# PARAMETERS
+NUM_ROOMS = 16
+PIT_RATE = 0.2
+BAT_RATE = 0.3
+ARROWS = 5
+SEED = 1701
+
+
+def run_game(ui: TextUI):
+    game = WumpusGame(num_rooms = NUM_ROOMS,
+                       pit_rate = PIT_RATE, 
+                       bat_rate = BAT_RATE, 
+                       arrows = ARROWS,
+                       seed = SEED)
+    
+    # SETUP
+    game.generate_rooms()
+    game.connect_rooms()
+    game.place_hazards()
+    game.place_player()
+
+    # INTRO
+
+    # CORE LOOP
+
+    # 
+
+def main():
+    ui = TextUI()
+    run_game(ui)
+
+if __name__ == "__main__":
+    main()
