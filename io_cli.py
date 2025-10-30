b11d66c0e6150ad8a309eab5a7a37872e0bdf549
Theodor Holmberg <35803316+egeltorp@users.noreply.github.com>
2025-10-30 16:38:21 +0100

    Add skeleton start + rich modules

---
+++ io_cli.py
@@ -0,0 +1,15 @@
+'''
+Module for text-based user interface for the Wumpus game.
+Utilizes the 'rich' library for fresh looking terminal output.
+Theodor Holmberg aka @egeltorp 2025
+'''
+
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+from rich.prompt import Prompt
+from rich.panel import Panel
+
+class TextUI:
+    def __init__(self):
+        pass
